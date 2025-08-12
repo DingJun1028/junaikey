871bbec921d1b64d94a966132396dff54163f181
DingJun Hong <dingjunhong1028@gmail.com>
2025-08-12 18:03:32 +0000

    --- type: Page title: JunAiKey SDK description: null icon: null createdA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,9 +32,9 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+          <div className="flex min-h-screen w-full">
             <AppSidebar />
-            <div className="flex flex-col">
+            <div className="flex flex-col flex-1">
               <AppHeader />
               <main className="flex flex-1 flex-col items-start bg-muted/40">
                 {children}
